f12cf45fd99fe9800656aa9d63bc8cc484af0937
Tim Haywood <tim@haywood.org>
2020-08-18 17:35:18 +1000

    move types into main to remove rollup to let typescript create the declaration file, so it can be consumed

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,20 @@
-/// <reference path="./types.d.ts" />
+type Points = Vector2D[];
+type Vector = [number, number, number?];
+type Vector2D = [number, number];
+type Vector3D = [number, number, number];
+type Color = [number, number, number, number];
+interface PathValue {}
+
+type SourceData = any[];
 
 // Global objects, attributes, and methods
 export const PathBase: PathValue = {};
+
+interface Key {
+  value: Value;
+  time: number;
+}
+
 export const KeyBase: Key = {
   value: "key value",
   time: 0,
@@ -12,6 +25,60 @@ export const PointsBase: Vector2D[] = [
   [100, 100],
   [0, 100],
 ];
+
+interface Project {
+  readonly fullPath: string;
+  readonly bitsPerChannel: "8" | "16" | "32";
+  readonly linearBlending: boolean;
+}
+
+export const ProjectBase: Project = {
+  fullPath: "path/to/project/file",
+  bitsPerChannel: "8",
+  linearBlending: true,
+};
+
+interface MarkerParam {
+  [id: string]: any;
+}
+
+interface Marker {
+  readonly time: number;
+  readonly index: number;
+  readonly duration: number;
+  readonly comment: string;
+  readonly chapter: string;
+  readonly url: string;
+  readonly frameTarget: string;
+  readonly eventCuePoint: boolean;
+  readonly cuePointName: string;
+  readonly parameters: MarkerParam;
+  readonly protectedRegion: boolean;
+}
+
+interface MarkerProperty {
+  readonly numKeys: number;
+  key(index: number | string): Marker;
+  nearestKey(t: number): Marker;
+}
+
+interface Comp {
+  readonly name: string;
+  readonly numLayers: number;
+  readonly activeCamera: Camera | null;
+  readonly width: number;
+  readonly height: number;
+  readonly duration: number;
+  readonly ntscDropFrame: boolean;
+  readonly displayStartTime: number;
+  readonly frameDuration: number;
+  readonly frameRate: number;
+  readonly shutterAngle: number;
+  readonly bgColor: Color;
+  readonly pixelAspect: number;
+  readonly marker?: MarkerProperty;
+  layer(indexOrOtherLayer: number | string | Layer, relIndex?: number): Layer;
+}
 
 export const CompBase: Comp = {
   name: "Comp Base",
@@ -30,10 +97,77 @@ export const CompBase: Comp = {
   layer: (indexOrOtherLayer, relIndex) => LayerBase,
 };
 
+interface PropertyGroup {
+  readonly name: string;
+}
+
 export const PropertyGroupBase: PropertyGroup = {
   name: "property group base",
 };
-export const ValueBase = 1;
+
+type Value =
+  | number
+  | Vector
+  | Vector2D
+  | Vector3D
+  | String
+  | []
+  | boolean
+  | PathValue;
+
+export const ValueBase: Value = 1;
+
+interface Property {
+  readonly value: Value;
+  readonly name: string;
+  readonly velocity: number | [];
+  readonly speed: number;
+  readonly numKeys: number;
+  readonly propertyIndex: number;
+  valueAtTime(time: number): Value;
+  velocityAtTime(time: number): number | [];
+  speedAtTime(time: number): number;
+  wiggle(
+    freq: number,
+    amp: number,
+    octaves?: number,
+    amp_mult?: number,
+    time?: number
+  ): Value;
+  temporalWiggle(
+    freq: number,
+    amp: number,
+    octaves?: number,
+    amp_mult?: number,
+    time?: number
+  ): Value;
+  smooth(width?: number, samples?: number, time?: number): Value;
+  loopIn(type?: loopType, numKeyframes?: number): Value;
+  loopOut(type?: loopType, numKeyframes?: number): Value;
+  loopInDuration(type?: loopType, duration?: number): Value;
+  loopOutDuration(type?: loopType, duration?: number): Value;
+  createPath?(
+    points: Points,
+    inTangents: Points | [],
+    outTangent: Points | [],
+    isClosed: boolean
+  ): PathValue;
+  key(indexOrName: number | string): Key | Marker;
+  propertyGroup(countUp: number): PropertyGroup;
+}
+
+interface PathProperty extends Property {
+  points(time?: number): Vector2D[];
+  inTangents(time?: number): Vector2D[];
+  outTangents(time?: number): Vector2D[];
+  isClosed(): boolean;
+  pointOnPath(percentage?: number, time?: number): Vector2D;
+  tangentOnPath(percentage?: number, time?: number): Vector2D;
+  normalOnPath(percentage?: number, time?: number): Vector2D;
+}
+
+type loopType = "cycle" | "pingpong" | "offset" | "continue";
+
 export const PropertyBase: PathProperty = {
   value: "property base string value",
   name: "property name",
@@ -65,6 +199,15 @@ export const PropertyBase: PathProperty = {
   normalOnPath: (percentage = 0.5, t = time) => [0, 0],
 };
 
+interface Transform extends PropertyGroup {
+  anchorPoint: Property;
+  position: Property;
+  scale: Property;
+  rotation: Property;
+  orientation: Property;
+  rotationX?: Property;
+}
+
 const TransformBase: Transform = {
   name: "Transform",
   anchorPoint: PropertyBase,
@@ -74,6 +217,18 @@ const TransformBase: Transform = {
   orientation: PropertyBase,
   rotationX: PropertyBase,
 };
+
+interface MaterialOptions extends PropertyGroup {
+  lightTransmission: Property;
+  castShadows: Property;
+  acceptsShadows: Property;
+  acceptsLights: Property;
+  ambient: Property;
+  diffuse: Property;
+  specular: Property;
+  shininess: Property;
+  metal: Property;
+}
 
 const MaterialBase: MaterialOptions = {
   name: "Material Property Group",
@@ -88,6 +243,17 @@ const MaterialBase: MaterialOptions = {
   metal: PropertyBase,
 };
 
+interface Effects extends PropertyGroup {}
+
+interface Masks extends PropertyGroup {}
+
+interface SourceRect {
+  readonly top: number;
+  readonly left: number;
+  readonly width: number;
+  readonly height: number;
+}
+
 export const SourceRectBase: SourceRect = {
   top: 0,
   left: 0,
@@ -95,10 +261,22 @@ export const SourceRectBase: SourceRect = {
   height: 100,
 };
 
+interface Effect {
+  active: boolean;
+  param(nameOrIndex: string | number): Property;
+}
+
 export const EffectBase: Effect = {
   active: true,
   param: (nameOrIndex) => PropertyBase,
 };
+
+interface Mask {
+  maskOpacity: Property;
+  maskFeather: Property;
+  maskExpansion: Property;
+  invert: boolean;
+}
 
 export const MaskBase: Mask = {
   maskOpacity: PropertyBase,
@@ -106,6 +284,59 @@ export const MaskBase: Mask = {
   maskFeather: PropertyBase,
   invert: false,
 };
+
+interface Light {
+  pointOfInterest: Vector3D;
+  intensity: number;
+  color: Color;
+  coneAngle: number;
+  coneFeather: number;
+  shadowDarkness: number;
+  shadowDiffusion: number;
+}
+
+interface Camera {}
+
+interface Layer {
+  readonly name: string;
+  readonly source?: Comp | Footage;
+  readonly width: number;
+  readonly height: number;
+  readonly index: number;
+  readonly parent?: Layer | Light | Camera;
+  readonly hasParent: boolean;
+  readonly inPoint: number;
+  readonly outPoint: number;
+  readonly startTime: number;
+  readonly hasVideo: boolean;
+  readonly hasAudio: boolean;
+  readonly active: boolean;
+  readonly enabled: boolean;
+  readonly audioActive?: boolean;
+  readonly audioLevels?: Property;
+  readonly timeRemap?: Property;
+  readonly marker?: MarkerProperty;
+  transform?: Transform;
+  materialOption?: MaterialOptions;
+  toComp(vec: Vector, time?: number): Vector;
+  fromComp(vec: Vector, time?: number): Vector;
+  toWorld(vec: Vector, time?: number): Vector;
+  toCompVec(vec: Vector, time?: number): Vector;
+  fromCompVec(vec: Vector, time?: number): Vector;
+  toWorldVec(vec: Vector, time?: number): Vector;
+  fromWorldVec(vec: Vector, time?: number): Vector;
+  fromCompToSurface(vec: Vector): Vector;
+  sourceTime?(time?: number): number;
+  sourceRectAtTime(time?: number, includeExtents?: boolean): SourceRect;
+  effect(nameOrIndex: number | string): Effect;
+  mask(nameOrIndex: number | string): Mask;
+  sampleImage(
+    point: Vector2D,
+    radius?: Vector2D,
+    postEffect?: boolean,
+    time?: number
+  ): Color;
+}
 
 export const LayerBase: Layer = {
   name: "layer base",
@@ -150,6 +381,22 @@ export function comp(index: number | string) {
 
 export const time: number = 0;
 export const colorDepth: number = 8;
+
+interface Footage {
+  readonly name: string;
+  readonly width?: number;
+  readonly height?: number;
+  readonly duration?: number;
+  readonly frameDuration?: number;
+  readonly ntscDropFrame?: boolean;
+  readonly pixelAspect?: number;
+  readonly sourceText?: string;
+  readonly sourceData?: SourceData[];
+  dataValue?(dataPath: []): number;
+  dataKeyCount?(dataPath: []): number;
+  dataKeyTimes?(dataPath: [], t0?: number, t1?: number): number[];
+  dataKeyValues?(dataPath: [], t0?: number, t1?: number): number[];
+}
 
 const FootageBase: Footage = {
   name: "Footage Item",
